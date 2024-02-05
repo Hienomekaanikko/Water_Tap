55c1eabf513513d17fd8908eb1b82935553707dd
Mikko Suokas <121874134+Hienomekaanikko@users.noreply.github.com>
2024-02-05 18:01:15 +0200

    Update waterglass.py

--- waterglass.py
+++ waterglass.py
@@ -1,6 +1,13 @@
 class WaterGlass:
+    def __init__(self):
+        self.dl = 0
     def fill_glass(self):
-        return f"glass is now fillingl"
-
+        self.dl += 1
+        if self.dl < 2:
+            return f"the glass is not full yet."
+        elif self.dl >= 2:
+            return f"the glass is full."
+        else:
+            return f"the glass is overflowing."
     def empty_glass(self):
         return f"glass is now empty."
