d734ee3cf18ef98fb3830eb576f2d83de5270b8d
Mikko Suokas <121874134+Hienomekaanikko@users.noreply.github.com>
2024-02-05 17:51:01 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import watertap
+import waterglass
+
+water_on = watertap.WaterTap()
+fill_glass = waterglass.WaterGlass()
+tap_on = True
+
+while tap_on:
+    water_on.open_tap()
+    ask = input("Fill a glass? ")
+    if ask == "yes":
+        print(fill_glass.fill_glass())
+    else:
+        water_on.close_tap()
+        tap_on = False
